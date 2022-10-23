e6f45d8a2c4748f383d73fd343007162b3c6d232
Nuseyba <n.kenadiid@gmail.com>
2022-10-23 18:58:22 +0100

    wrote my quesitons in javascript

---
+++ Assets/script.js
@@ -0,0 +1,27 @@
+let storeQuiz = document.getElementById ("quiz")
+let storeResults = document.getElementById ("results")
+let beginButton = document.getElementById ("begin-quiz")
+
+let myQuestions = [
+    {
+        question : "How do you add comments in HTML?",
+        answers : {
+            a : "<!--",
+            b : "<..",
+            c : "//"
+        },
+     rightAnswer : "a"   
+     },
+
+     {
+        question: "How do you stack contents?",
+        answers : {
+            a : "justify-content",
+            b : "text-align",
+            c : "flex-direction",
+        },
+     rightAnswer: "c"   
+     },
+     
+]
+    
