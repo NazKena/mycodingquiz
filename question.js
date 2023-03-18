df9556470c2df81faf5e73785e247d794b3b06f9
Nuseyba <n.kenadiid@gmail.com>
2023-03-18 10:02:17 +0000

    questions working

---
+++ question.js
@@ -0,0 +1,32 @@
+let myQuestions = [
+    {
+        question : "How do you add comments in HTML?",
+        answers : {
+            a : "<!--",
+            b : "<..",
+            c : "//"
+        },
+     rightAnswer : "a"   
+     },
+
+     {
+        question: "How do you stack contents?",
+        answers : {
+            a : "justify-content",
+            b : "text-align",
+            c : "flex-direction",
+        },
+     rightAnswer: "c",   
+     },
+
+     {
+        question: "What does this JavaScript comparison operator mean ('!=')?",
+        answers : {
+            a : "equal to",
+            b : "not equal to",
+            c : "error",
+        },
+        rightAnswer : "b",
+     }
+
+]
--- script.js
+++ script.js
@@ -1,90 +1,140 @@
-function buildQuiz() {
-  
-  document.getElementById("showQuiz").style.visibility = "hidden"
-  const output = [];
+// Questions will be asked
+const Questions = [{
+    id: 0,
+    q: "How do you add comments in HTML?",
+    a: [{ text: "<!--", isCorrect: true },
+        { text: "<..", isCorrect: false },
+        { text: "//", isCorrect: false },
+        { text: "--!!", isCorrect: false }
+    ]
 
-  myQuestions.forEach((currentQuestion, questionNumber) => {
-    const answers = [];
+},
+{
+    id: 1,
+    q: "How do you stack contents?",
+    a: [{ text: "justify-content", isCorrect: false,},
+        { text: "text-align", isCorrect: false },
+        { text: "flex-direction", isCorrect: true },
+        { text: "display-stack", isCorrect: false }
+    ]
 
-    for (letter in currentQuestion.answers) {
-      answers.push(
-        `<label>
-        <input type="radio" name="question${questionNumber}" value="${letter}">
-        ${letter} :
-        ${currentQuestion.answers[letter]}
-      </label>`
-      );
-    }
+},
+{
+    id: 2,
+    q:  "What does this JavaScript comparison operator mean ('!=')?",
+    a: [{ text: "equal to", isCorrect: false },
+        { text: "not equal to", isCorrect: true },
+        { text: "error", isCorrect: true },
+        { text: "not correct", isCorrect: false }
+    ]
 
-    output.push(
-      `<div class="question"> ${currentQuestion.question} </div>
-    <div class="answers"> ${answers.join("")} </div>`
-    );
-  });
-
-  quizContainer.innerHTML = output.join("");
 }
-
-let myQuestions = [
-    {
-        question : "How do you add comments in HTML?",
-        answers : {
-            a : "<!--",
-            b : "<..",
-            c : "//"
-        },
-     rightAnswer : "a"   
-     },
-
-     {
-        question: "How do you stack contents?",
-        answers : {
-            a : "justify-content",
-            b : "text-align",
-            c : "flex-direction",
-        },
-     rightAnswer: "c",   
-     },
-
-     {
-        question: "What does this JavaScript comparison operator mean ('!=')?",
-        answers : {
-            a : "equal to",
-            b : "not equal to",
-            c : "error",
-        },
-        rightAnswer : "b",
-     }
-
 ]
 
 
+// Set start
+var start = true;
+
+// Iterate
+function iterate(id) {
+
+// Here we are displaying the result
+var result = document.getElementsByClassName("result");
+result[0].innerText = "";
+
+// This is where we get the question
+const question = document.getElementById("question");
 
 
+// Setting the question text
+question.innerText = Questions[id].q;
+
+// Options for the questions are retrieved
+const selection1 = document.getElementById('selection1');
+const selection2 = document.getElementById('selection2');
+const selection3 = document.getElementById('selection3');
+const selection4 = document.getElementById('selection4');
 
 
+// Providing option text
+selection1.innerText = Questions[id].a[0].text;
+selection2.innerText = Questions[id].a[1].text;
+selection3.innerText = Questions[id].a[2].text;
+selection4.innerText = Questions[id].a[3].text;
 
+// Providing the true or false value to the options
+selection1.value = Questions[id].a[0].isCorrect;
+selection2.value = Questions[id].a[1].isCorrect;
+selection3.value = Questions[id].a[2].isCorrect;
+selection4.value = Questions[id].a[3].isCorrect;
 
-function startTimer(duration, display) {
-  var timer = duration, minutes, seconds;
-  setInterval(function () {
-      minutes = parseInt(timer / 60, 10)
-      seconds = parseInt(timer % 60, 10);
+var selected = "";
 
-      minutes = minutes < 10 ? "0" + minutes : minutes;
-      seconds = seconds < 10 ? "0" + seconds : seconds;
+// Show selection for op1
+selection1.addEventListener("click", () => {
+    selection1.style.backgroundColor = "lightgoldenrodyellow";
+    selection2.style.backgroundColor = "lightskyblue";
+    selection3.style.backgroundColor = "lightskyblue";
+    selection4.style.backgroundColor = "lightskyblue";
+    selected = selection1.value;
+})
 
-      display.textContent = minutes + ":" + seconds;
+// Show selection for op2
+selection2.addEventListener("click", () => {
+    selection1.style.backgroundColor = "lightskyblue";
+    selection2.style.backgroundColor = "lightgoldenrodyellow";
+    selection3.style.backgroundColor = "lightskyblue";
+    selection4.style.backgroundColor = "lightskyblue";
+    selected = selection2.value;
+})
 
-      if (--timer < 0) {
-          timer = 0;
-         
-      }
-  }, 1000);
+// Show selection for op3
+selection3.addEventListener("click", () => {
+    selection1.style.backgroundColor = "lightskyblue";
+    selection2.style.backgroundColor = "lightskyblue";
+    selection3.style.backgroundColor = "lightgoldenrodyellow";
+    selection4.style.backgroundColor = "lightskyblue";
+    selected = selection3.value;
+})
+
+// Show selection for op4
+selection4.addEventListener("click", () => {
+    selection1.style.backgroundColor = "lightskyblue";
+    selection2.style.backgroundColor = "lightskyblue";
+    selection3.style.backgroundColor = "lightskyblue";
+    selection4.style.backgroundColor = "lightgoldenrodyellow";
+    selected = selection4.value;
+})
+
+// Grabbing the evaluate button
+const evaluate = document.getElementsByClassName("evaluate");
+
+// Evaluate method
+evaluate[0].addEventListener("click", () => {
+    if (selected == "true") {
+        result[0].innerHTML = "True";
+        result[0].style.color = "green";
+    } else {
+        result[0].innerHTML = "False";
+        result[0].style.color = "red";
+    }
+})
 }
 
-window.onload = function () {
-  var time = 600 / 2, 
-      display = document.querySelector('#safeTimerDisplay');
-  startTimer(time, display);
-};
+if (start) {
+iterate("0");
+}
+
+// Next button and method
+const next = document.getElementsByClassName('next')[0];
+var id = 0;
+
+next.addEventListener("click", () => {
+start = false;
+if (id < 2) {
+    id++;
+    iterate(id);
+    console.log(id);
+}
+
+})
