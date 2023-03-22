91f5a74d14dffce46e860ae4e9ab3322210088de
Nuseyba <n.kenadiid@gmail.com>
2023-03-22 11:34:55 +0000

    done by selecting requiremtnta in my javascript

---
+++ script.js
@@ -0,0 +1,23 @@
+const start_btn = document.querySelector(".start_btn button");
+const info_box = document.querySelector(".info_box");
+const exit_btn = info_box.querySelector(".buttons .quit");
+const continue_btn = info_box.querySelector(".buttons .restart");
+const quiz_box = document.querySelector(".quiz_container");
+
+
+start_btn.onclick = ()=>{
+    info_box.classList.add("activeInfo"); 
+}
+
+exit_btn.onclick = ()=>{
+    info_box.classList.remove("activeInfo"); 
+}
+
+continue_btn.onclick = ()=>{
+    info_box.classList.remove("activeInfo"); 
+    quiz_box.classList.add("activeQuiz"); 
+    showQuetions(0); 
+    queCounter(1); 
+    startTimer(15); 
+    startTimerLine(0); 
+}
